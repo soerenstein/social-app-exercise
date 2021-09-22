cb09a527da7b34bac6c5f1259013bba161f4acf3
Sören Stein <stein.soeren@gmx.de>
2021-09-22 16:50:36 +0200

    Add model and replace resolvers

--- index.js
+++ index.js
@@ -3,15 +3,30 @@ const gql = require('graphql-tag')
 const mongoose = require('mongoose')
 require('dotenv').config()
 
+const Post = require('./models/Post')
+
 const typeDefs = gql`
+  type Post {
+    id: ID!
+    body: String!
+    createdAt: String!
+    username: String!
+  }
   type Query {
-    sayHi: String
+    getPosts: [Post]
   }
 `
 
 const resolvers = {
   Query: {
-    sayHi: () => 'Hello World!',
+    async getPosts() {
+      try {
+        const posts = await Post.find()
+        return posts
+      } catch (err) {
+        throw new Error(err)
+      }
+    },
   },
 }
 
