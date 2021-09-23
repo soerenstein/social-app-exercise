ebdb089d00e1755a25ef6e36c9cd350fc99115ba
Sören Stein <stein.soeren@gmx.de>
2021-09-23 13:46:53 +0200

    Add registerInput and prepare Mutation

--- graphql/typeDefs.js
+++ graphql/typeDefs.js
@@ -7,7 +7,23 @@ module.exports = gql`
     createdAt: String!
     username: String!
   }
+  type User {
+    id: ID!
+    email: String!
+    token: String!
+    username: String!
+    createdAt: String!
+  }
+  input RegisterInput {
+    username: String!
+    passwort: String!
+    confirmPassword: String!
+    email: String!
+  }
   type Query {
     getPosts: [Post]
+  }
+  type Mutation {
+    register(registerInput: RegisterInput): User!
   }
 `
