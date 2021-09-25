9f044138de4cd4bbcaec206a6a2541c806a1a777
Sören Stein <stein.soeren@gmx.de>
2021-09-25 20:07:48 +0200

    Functionality to delete comments

--- graphql/resolvers/comments.js
+++ graphql/resolvers/comments.js
@@ -1,4 +1,4 @@
-const { UserInputError } = require('apollo-server')
+const { AuthenticationError, UserInputError } = require('apollo-server')
 
 const Post = require('../../models/Post')
 const checkAuth = require('../../util/checkAuth')
@@ -24,6 +24,24 @@ module.exports = {
         await post.save()
         return post
       } else throw new UserInputError('Post nicht gefunden')
+    },
+    async deleteComment(_, { postId, commentId }, context) {
+      const { username } = checkAuth(context)
+
+      const post = await Post.findById(postId)
+
+      if (post) {
+        const commentIndex = post.comments.findIndex((c) => c.id === commentId)
+        if (post.comments[commentIndex].username === username) {
+          post.comments.splice(commentIndex, 1)
+          await post.save()
+          return post
+        } else {
+          throw new AuthenticationError('Unzulässiger Befehl')
+        }
+      } else {
+        throw new UserInputError('Post nicht gefunden')
+      }
     },
   },
 }
